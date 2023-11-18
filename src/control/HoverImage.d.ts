4bbe37500f56af982177560189adbfe10a565ab0
Johannes <stopwhispering314@gmail.com>
2023-11-18 14:38:34 +0100

    more hover imgs

--- src/control/HoverImage.d.ts
+++ src/control/HoverImage.d.ts
@@ -1,8 +1,4 @@
 
-import Event from "sap/ui/base/Event";
-import { PropertyBindingInfo } from "sap/ui/base/ManagedObject";
-import { $ImageSettings } from "sap/m/Image";
-
 declare module "./HoverImage" {
 
     export interface HoverImage$HoverEventParameters {
